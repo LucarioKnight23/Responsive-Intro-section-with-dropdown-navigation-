d7a199b3e6f06f90a4d5c0957c6589f642550f32
LucarioKnight23 <107028244+LucarioKnight23@users.noreply.github.com>
2022-06-16 19:55:26 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+}
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
+
+let el = document.getElementsByClassName("image");
+
+if (screen.width > 1000) {
+  el[0].src = "images/image-hero-desktop.png";
+}
+
+else {
+  el[0].src = "images/image-hero-mobile.png";
+}
